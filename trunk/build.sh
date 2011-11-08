ddebb2245f449706bc7f4ef6b0d0e04ad9b7762c
encaisse@gmail.com <encaisse@gmail.com@927aa451-fb1f-0410-ae3a-b9e4d2c0957c>
2011-11-08 22:09:30 +0000

    Fix build script for os x

--- trunk/build.sh
+++ trunk/build.sh
@@ -3,7 +3,7 @@ APP_NAME=phplangeditor
 # building
 mkdir build
 mkdir build/chrome
-# todo ignore CVS files and repositories
+# todo ignore CVS, SVN files and repositories
 zip -rq build/chrome/$APP_NAME.jar chrome/*
 cp install.* build
 zip -rq $APP_NAME.xpi build/*
